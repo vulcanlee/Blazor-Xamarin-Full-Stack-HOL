2423dff38c5dde7e2a2e79ec431dace9ce644d03
Vulcan <vulcan.lee@gmail.com>
2021-01-25 22:20:03 +0800

    OnCallPhoneService

---
+++ Src/FrontMobile/Business/Services/OnCallPhoneService.cs
@@ -0,0 +1,49 @@
+﻿using CommonLibrary.Helpers;
+using CommonLibrary.Helpers.WebAPIs;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using DataTransferObject.DTOs;
+
+namespace Business.Services
+{
+    public class OnCallPhoneService : BaseWebAPI<OnCallPhoneDto>
+    {
+        public OnCallPhoneService()
+            : base()
+        {
+            this.Url = "/api/OnCallPhone";
+            this.Host = LOBGlobal.APIEndPointHost;
+            SetDefaultPersistentBehavior();
+        }
+
+        void SetDefaultPersistentBehavior()
+        {
+            ApiResultIsCollection = true;
+            PersistentStorage = ApiResultIsCollection ? PersistentStorage.Collection : PersistentStorage.Single;
+        }
+
+        public async Task<APIResult> GetAsync()
+        {
+            #region 指定此次呼叫 Web API 要執行參數
+            Token = "";
+            ApiResultIsCollection = true;
+            EnctypeMethod = EnctypeMethod.JSON;
+            Route = $"";
+            #endregion
+
+            #region 要傳遞的參數
+            WebQueryDictionary dic = new WebQueryDictionary();
+            #endregion
+
+            APIResult apiResult = await this.SendAsync(dic, HttpMethod.Get, CancellationToken.None);
+            SetDefaultPersistentBehavior();
+
+            return apiResult;
+        }
+    }
+}
