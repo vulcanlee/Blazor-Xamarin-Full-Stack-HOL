e53baba3da1604f781e27427d5805da8519a8ff9
Vulcan <vulcan.lee@gmail.com>
2021-04-06 18:23:21 +0800

    更新介面說明

--- Src/BackendStarter2/Backend/Interfaces/IDataGrid.cs
+++ Src/BackendStarter2/Backend/Interfaces/IDataGrid.cs
@@ -4,7 +4,14 @@ namespace Backend.Interfaces
 {
     public interface IDataGrid
     {
+        /// <summary>
+        /// 強制更新 Grid 從 Adapter 讀取記錄
+        /// </summary>
         void RefreshGrid();
+        /// <summary>
+        /// sfGrid 是否已經存在
+        /// </summary>
+        /// <returns></returns>
         bool GridIsExist();
         Task InvokeGridAsync(string actionName);
     }
--- Src/BackendStarter2/Backend/Interfaces/IRazorPage.cs
+++ Src/BackendStarter2/Backend/Interfaces/IRazorPage.cs
@@ -5,7 +5,15 @@ namespace Backend.Interfaces
 {
     public interface IRazorPage
     {
+        /// <summary>
+        /// 呼叫元件的 StateHasChanged()
+        /// </summary>
         void NeedRefresh();
+        /// <summary>
+        /// 在 UI 執行緒下執行委派方法
+        /// </summary>
+        /// <param name="action"></param>
+        /// <returns></returns>
         Task NeedInvokeAsync(Action action);
     }
 }
