e5bde6654aedf99f1be871528a282b5671778864
Vulcan <vulcan.lee@gmail.com>
2021-05-30 01:07:01 +0800

    登入驗證要檢查帳號是否已經被停用

--- Src/BackendStarter3/Backend/Pages/Login.cshtml.cs
+++ Src/BackendStarter3/Backend/Pages/Login.cshtml.cs
@@ -64,13 +64,20 @@ namespace Backend.Pages
             {
                 Msg = message;
                 logger.LogInformation($"使用者 ({Username} / {Password}) 登入失敗");
+                return Page();
             }
-            else
-            {
-                string returnUrl = Url.Content("~/");
 
-                #region 加入這個使用者需要用到的 宣告類型 Claim Type
-                var claims = new List<Claim>
+            if (user.Status == false)
+            {
+                Msg = $"使用者 {user.Account} 已經被停用";
+                logger.LogInformation($"{Msg}");
+                return Page();
+            }
+
+            string returnUrl = Url.Content("~/");
+
+            #region 加入這個使用者需要用到的 宣告類型 Claim Type
+            var claims = new List<Claim>
                 {
                     new Claim(ClaimTypes.Role, "User"),
                     new Claim(ClaimTypes.NameIdentifier, user.Account),
@@ -80,63 +87,62 @@ namespace Backend.Pages
                     new Claim(MagicHelper.MenuRoleNameClaim, user.MenuRole?.Name),
                 };
 
-                #region 若為 開發人員，加入 開發人員 專屬的角色
-                if (MagicHelper.開發者帳號 == Username.ToLower())
-                {
-                    claims.Add(new Claim(ClaimTypes.Role, MagicHelper.開發者的角色聲明));
-                }
-                #endregion
+            #region 若為 開發人員，加入 開發人員 專屬的角色
+            if (MagicHelper.開發者帳號 == Username.ToLower())
+            {
+                claims.Add(new Claim(ClaimTypes.Role, MagicHelper.開發者的角色聲明));
+            }
+            #endregion
 
-                #region 加入該使用者需要加入的相關角色
-                var menuDatas = user.MenuRole.MenuData
-                     .Where(x => x.Enable == true).ToList();
+            #region 加入該使用者需要加入的相關角色
+            var menuDatas = user.MenuRole.MenuData
+                 .Where(x => x.Enable == true).ToList();
 
-                foreach (var item in menuDatas)
+            foreach (var item in menuDatas)
+            {
+                if (item.IsGroup == false)
                 {
-                    if (item.IsGroup == false)
+                    // 避免使用者自己加入一個 開發人員專屬 的角色
+                    if (item.CodeName != "/" &&
+                        item.CodeName.ToLower() != MagicHelper.開發者的角色聲明)
                     {
-                        // 避免使用者自己加入一個 開發人員專屬 的角色
-                        if (item.CodeName != "/" && 
-                            item.CodeName.ToLower() != MagicHelper.開發者的角色聲明)
-                        {
-                            claims.Add(new Claim(ClaimTypes.Role, item.CodeName));
-                        }
+                        claims.Add(new Claim(ClaimTypes.Role, item.CodeName));
                     }
                 }
-                #endregion
-                #endregion
-
-                #region 建立 宣告式身分識別
-                // ClaimsIdentity類別是宣告式身分識別的具體執行, 也就是宣告集合所描述的身分識別
-                var claimsIdentity = new ClaimsIdentity(
-                    claims, MagicHelper.CookieAuthenticationScheme);
-                #endregion
-
-                #region 建立關於認證階段需要儲存的狀態
-                var authProperties = new AuthenticationProperties
-                {
-                    IsPersistent = true,
-                    RedirectUri = this.Request.Host.Value
-                };
-                #endregion
-
-                #region 進行使用登入
-                try
-                {
-                    await HttpContext.SignInAsync(
-                    MagicHelper.CookieAuthenticationScheme,
-                    new ClaimsPrincipal(claimsIdentity),
-                    authProperties);
-                }
-                catch (Exception ex)
-                {
-                    var msg = ex.Message;
-                }
-                #endregion
-
-                logger.LogInformation($"使用者 ({Username}) 登入成功");
-                return LocalRedirect(returnUrl);
             }
+            #endregion
+            #endregion
+
+            #region 建立 宣告式身分識別
+            // ClaimsIdentity類別是宣告式身分識別的具體執行, 也就是宣告集合所描述的身分識別
+            var claimsIdentity = new ClaimsIdentity(
+                claims, MagicHelper.CookieAuthenticationScheme);
+            #endregion
+
+            #region 建立關於認證階段需要儲存的狀態
+            var authProperties = new AuthenticationProperties
+            {
+                IsPersistent = true,
+                RedirectUri = this.Request.Host.Value
+            };
+            #endregion
+
+            #region 進行使用登入
+            try
+            {
+                await HttpContext.SignInAsync(
+                MagicHelper.CookieAuthenticationScheme,
+                new ClaimsPrincipal(claimsIdentity),
+                authProperties);
+            }
+            catch (Exception ex)
+            {
+                var msg = ex.Message;
+            }
+            #endregion
+
+            logger.LogInformation($"使用者 ({Username}) 登入成功");
+            return LocalRedirect(returnUrl);
             return Page();
         }
     }
