ed7d34029a47ccf0b7ef87258d566ee4f0131863
Vulcan <vulcan.lee@gmail.com>
2021-06-12 22:04:09 +0800

    ASP.NET Core 基本功能操作 - 從空白專案建立 Blazor 應用

--- Examples/AC01/AC01/Startup.cs
+++ Examples/AC01/AC01/Startup.cs
@@ -16,6 +16,10 @@ namespace AC01
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            #region 這裡加入 Blazor 需要用到的相依性注入宣告
+            services.AddRazorPages();
+            services.AddServerSideBlazor();
+            #endregion
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -26,14 +30,18 @@ namespace AC01
                 app.UseDeveloperExceptionPage();
             }
 
+            #region 要加入這個敘述，以便可以執行 SignalR 的 JavaScript 程式
+            app.UseStaticFiles();
+            #endregion
+
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                #region 啟動 SignalR 服務與註冊最終路由端點
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+                #endregion
             });
         }
     }
