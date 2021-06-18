0ec7fbc682312956e2b486249adae32a587ca7c0
Vulcan <vulcan.lee@gmail.com>
2021-06-18 22:20:17 +0800

    ASP.NET Core 應該具備知識 - 日誌記錄

---
+++ Examples/AC07/AC07/Controllers/WebLogController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AC07.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WebLogController : ControllerBase
+    {
+        public WebLogController(ILogger<WebLogController> logger)
+        {
+            Logger = logger;
+        }
+
+        public ILogger<WebLogController> Logger { get; }
+
+        [HttpGet]
+        public async Task<string> Get()
+        {
+            StringBuilder result = new StringBuilder();
+
+            var client = new HttpClient();
+
+            try
+            {
+                Stopwatch stopwatch = new Stopwatch();
+                stopwatch.Start();
+                
+                Logger.LogDebug($"開始讀取 Google 網頁資料");
+                string message = await client.GetStringAsync("http://www.google.com");
+                Logger.LogInformation($"完成讀取 Google 網頁資料");
+                stopwatch.Stop();
+                result.Append($"讀取 google 資料共花費 {stopwatch.ElapsedMilliseconds} ms" + Environment.NewLine);
+                
+                Logger.LogInformation($"開始讀取 m1cr050ft 網頁資料");
+                message = await client.GetStringAsync("http://www.m1cr050ft.com");
+                Logger.LogInformation($"完成讀取 m1cr050ft 網頁資料");
+                result.Append($"讀取 micr0s0ft 資料共花費 {stopwatch.ElapsedMilliseconds} ms" + Environment.NewLine);
+            }
+            catch (Exception ex)
+            {
+                Logger.LogError(ex, $"使用 HttpClient 發生了問題");
+            }
+
+            return result.ToString();
+        }
+    }
+}
---
+++ Examples/AC07/AC07/UnhandledExceptionMiddleware.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Threading.Tasks;
+
+namespace AC07
+{
+    public class UnhandledExceptionMiddleware
+    {
+        private readonly ILogger logger;
+        private readonly RequestDelegate next;
+
+        public UnhandledExceptionMiddleware(ILogger<UnhandledExceptionMiddleware> logger, RequestDelegate next)
+        {
+            this.logger = logger;
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+                if(IsSuccessStatusCode(context.Response))
+                {
+                    logger.LogDebug($"HTTP 回應成功 {context.Response.StatusCode}");
+                }
+                else
+                {
+                    logger.LogWarning($"HTTP 回應不是成功代碼 {context.Response.StatusCode}");
+                }
+            }
+            catch (Exception exception)
+            {
+                logger.LogError(exception,
+                    $"Request {context.Request?.Method}: {context.Request?.Path.Value} failed");
+            }
+        }
+        public bool IsSuccessStatusCode(HttpResponse response)
+        {
+            return response.StatusCode >= 200 && response.StatusCode <= 299;
+        }
+    }
+}
