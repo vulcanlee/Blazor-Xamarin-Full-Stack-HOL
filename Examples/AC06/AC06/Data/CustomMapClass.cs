953285dd04642004127ad6321d0c74e20bccf105
Vulcan <vulcan.lee@gmail.com>
2021-06-18 15:06:00 +0800

    ASP.NET Core 應該具備知識 - 選項模式

---
+++ Examples/AC06/AC06/Data/CustomMapClass.cs
@@ -0,0 +1,16 @@
+﻿namespace AC06.Data
+{
+    public class CustomMapClass
+    {
+        public string MyEnglishName { get; set; }
+        public bool IsNETDeveloper { get; set; }
+        public int NETDeveloperYear { get; set; }
+        public Nest Nest { get; set; }
+    }
+
+    public class Nest
+    {
+        public bool IsNETDeveloper { get; set; }
+        public int NETDeveloperYear { get; set; }
+    }
+}
