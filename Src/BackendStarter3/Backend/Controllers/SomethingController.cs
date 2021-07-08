111746473ee360581a5792bdc0b8ffc24b545f5d
Vulcan <vulcan.lee@gmail.com>
2021-07-08 20:47:57 +0800

    加入測試用 API

---
+++ Src/BackendStarter3/Backend/Controllers/SomethingController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SomethingController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<string> GetAsync()
+        {
+            await Task.Delay(3000);
+            return "Hello~~";
+        }
+    }
+}
