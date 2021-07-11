46d9f596e5b5974b515f8b6c6df0cdf16ef4b799
Vulcan <vulcan.lee@gmail.com>
2021-07-11 11:27:04 +0800

    加入非同步的測試用 API

---
+++ Src/BackendStarter3/Backend/Controllers/DoAsyncWorkController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoAsyncWorkController : ControllerBase
+    {
+        [HttpGet("{waitPeriodOfTime}")]
+        public async Task<string> GetAsync([FromRoute] int waitPeriodOfTime)
+        {
+            await Task.Delay(waitPeriodOfTime);
+            return "Hello~~";
+        }
+    }
+}
