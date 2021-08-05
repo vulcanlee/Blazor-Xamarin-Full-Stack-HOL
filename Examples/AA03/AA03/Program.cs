bb00ea718db77707acbbaf99fba994f50193d211
Vulcan <vulcan.lee@gmail.com>
2021-08-05 19:55:18 +0800

    FormUrlEncodedContent

---
+++ Examples/AA03/AA03/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AA03
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var result = await LoginPostAsync(new LoginRequestDto()
+            {
+                Account = "user1",
+                Password = "123"
+            });
+            Console.WriteLine(result);
+        }
+        private static async Task<string> LoginPostAsync(LoginRequestDto loginRequestDto)
+        {
+            string result = "";
+            using (HttpClient client = new HttpClient())
+            {
+                string endPoint = $"https://localhost:5001/api/PassParameter/FromForm";
+                HttpResponseMessage response = null;
+
+                #region 使用 FormUrlEncodedContent 產生要 Post 的資料
+                //// 方法一： 使用字串名稱用法
+                //var formData = new FormUrlEncodedContent(new[] {
+                //    new KeyValuePair<string, string>(nameof(loginRequestDto.Account), loginRequestDto.Account.ToString()),
+                //    new KeyValuePair<string, string>(nameof(loginRequestDto.Password), loginRequestDto.Password),
+                //});
+
+                // 方法二： 強型別用法
+                // https://docs.microsoft.com/zh-tw/dotnet/csharp/language-reference/keywords/nameof
+                Dictionary<string, string> formDataDictionary = new Dictionary<string, string>()
+                {
+                    {nameof(loginRequestDto.Account), loginRequestDto.Account.ToString() },
+                    {nameof(loginRequestDto.Password), loginRequestDto.Password.ToString() },
+                };
+
+                #endregion       
+                
+                // https://msdn.microsoft.com/zh-tw/library/system.net.http.formurlencodedcontent(v=vs.110).aspx
+                using (var fooContent = new FormUrlEncodedContent(formDataDictionary))
+                {
+                    response = await client.PostAsync(endPoint, fooContent);
+                }
+
+                result = await response.Content.ReadAsStringAsync();
+            }
+
+            return result;
+        }
+    }
+    #region 呼叫 Web API 會用到的類別
+    public class LoginRequestDto
+    {
+        [Required]
+        public string Account { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+    #endregion
+}
---
+++ Src/BackendStarter4/Backend/Controllers/PassParameterController.cs
@@ -0,0 +1,35 @@
+﻿using DTOs.DataModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PassParameterController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] LoginRequestDto loginRequestDTO)
+        {
+            await Task.Yield();
+            return Ok(loginRequestDTO);
+        }
+
+        [HttpPost]
+        [Route("FromForm")]
+        public async Task<IActionResult> PostFromForm([FromForm] LoginRequestDto loginRequestDTO)
+        {
+            await Task.Yield();
+            loginRequestDTO.Account = $"Hi {loginRequestDTO.Account}";
+            return Ok(loginRequestDTO);
+        }
+
+    }
+}
