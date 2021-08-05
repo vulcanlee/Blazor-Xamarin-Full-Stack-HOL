1766ee8abbce1e8e9c9d70eafc7f93f341a83baf
Vulcan <vulcan.lee@gmail.com>
2021-08-05 22:06:40 +0800

    加入 GetStatusController

---
+++ Src/BackendStarter4/Backend/Controllers/GetStatusController.cs
@@ -0,0 +1,38 @@
+﻿using DTOs.DataModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetStatusController : ControllerBase
+    {
+        [HttpGet("OK")]
+        public IActionResult GetOKAsync()
+        {
+            return Ok($"你得到 Ok 狀態");
+        }
+        [HttpGet("NotFound")]
+        public IActionResult GetNotFound()
+        {
+            return NotFound($"你得到 NotFound 狀態");
+        }
+        [HttpGet("BadRequest")]
+        public IActionResult GetBadRequestFound()
+        {
+            return BadRequest($"你得到 BadRequest 狀態");
+        }
+        [HttpGet("Unauthorized")]
+        public IActionResult GetUnauthorized()
+        {
+            return Unauthorized($"你得到 Unauthorized 狀態");
+        }
+    }
+}
