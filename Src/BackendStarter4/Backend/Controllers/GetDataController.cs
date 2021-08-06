3845c873d2ca61bd306e411a4234b123e16ee28c
Vulcan <vulcan.lee@gmail.com>
2021-08-06 12:53:19 +0800

    取得檔案 PhysicalFileResult

--- Src/BackendStarter4/Backend/Controllers/GetDataController.cs
+++ Src/BackendStarter4/Backend/Controllers/GetDataController.cs
@@ -1,11 +1,13 @@
 ﻿using DTOs.DataModels;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Net.Http.Headers;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
@@ -17,6 +19,12 @@ namespace Backend.Controllers
     [ApiController]
     public class GetDataController : ControllerBase
     {
+        private readonly IWebHostEnvironment hostingEnvironment;
+
+        public GetDataController(IWebHostEnvironment hostingEnvironment)
+        {
+            this.hostingEnvironment = hostingEnvironment;
+        }
         [HttpGet("Cookie")]
         public async Task<IActionResult> GetCookie()
         {
@@ -32,6 +40,43 @@ namespace Backend.Controllers
             HttpContext.Response.Cookies.Append("YourName", "Vulcan", cookieOptions);
 
             return Ok("Cookie");
+        }
+        [HttpGet("Header")]
+        public async Task<IActionResult> GetHeader()
+        {
+            await Task.Yield();
+
+            Response.Headers.Add("X-Total-Count", "20");
+
+            return Ok("Header");
+        }
+        [HttpGet("Object")]
+        public async Task<IActionResult> GetObject()
+        {
+            await Task.Yield();
+
+            LoginRequestDto loginRequestDto = new()
+            {
+                Account = "Vulcan",
+                Password = "Lee",
+            };
+
+            return Ok(loginRequestDto);
+        }
+        [HttpGet("File")]
+        public async Task<IActionResult> GetFile()
+        {
+            await Task.Yield();
+
+            string wwwPath = hostingEnvironment.WebRootPath;
+            string contentPath = hostingEnvironment.ContentRootPath;
+            string fileName = "site.css";
+            string filePath = Path.Combine(wwwPath, "css", fileName);
+            // https://developer.mozilla.org/zh-TW/docs/Web/HTTP/Basics_of_HTTP/MIME_types#%E9%87%8D%E8%A6%81%E7%9A%84mime%E9%A1%9E%E5%88%A5
+            string contentType = "text/css";
+            PhysicalFileResult fileResult = new PhysicalFileResult(filePath, contentType);
+
+            return fileResult;
         }
     }
 }
