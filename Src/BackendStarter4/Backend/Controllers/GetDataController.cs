32e1a7bdd173b700dcfce32a3f3d7af2ce473afb
Vulcan <vulcan.lee@gmail.com>
2021-08-06 12:21:18 +0800

    傳送 Cookie

---
+++ Src/BackendStarter4/Backend/Controllers/GetDataController.cs
@@ -0,0 +1,37 @@
+﻿using DTOs.DataModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GetDataController : ControllerBase
+    {
+        [HttpGet("Cookie")]
+        public async Task<IActionResult> GetCookie()
+        {
+            await Task.Yield();
+            var cookieOptions = new CookieOptions()
+            {
+                Path = "/",
+                HttpOnly = false,
+                IsEssential = true, //<- there
+                Expires = DateTime.Now.AddMonths(1),
+                Secure = true,
+            };
+            HttpContext.Response.Cookies.Append("YourName", "Vulcan", cookieOptions);
+
+            return Ok("Cookie");
+        }
+    }
+}
---
+++ Src/BackendStarter4/Backend/Controllers/UploadController.cs
@@ -0,0 +1,24 @@
+﻿using DTOs.DataModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UploadController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult PostAsync(IFormFile file, [FromForm] string description)
+        {
+            return Ok($"你得到 Ok 狀態");
+        }
+    }
+}
