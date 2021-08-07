6f1d20c40214acc20f98b1b01e72d13c840ec75d
Vulcan <vulcan.lee@gmail.com>
2021-08-07 20:28:20 +0800

    沒有經過身分驗證，呼叫有授權才能存取的 API

---
+++ Examples/AA12/AA12/Program.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AA12
+{
+    /// <summary>
+    /// 沒有經過身分驗證，呼叫有授權才能存取的 API
+    /// </summary>
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var result = await OnlyUserGetAsync();
+            Console.WriteLine(result.Message);
+            Console.WriteLine(JsonConvert.SerializeObject(result, Formatting.Indented));
+        }
+        private static async Task<OnlyUserDto> OnlyUserGetAsync()
+        {
+            OnlyUserDto onlyUserDto;
+            using (HttpClient client = new HttpClient())
+            {
+                string endPoint = $"https://localhost:5001/api/OnlyUser";
+                HttpResponseMessage response = null;
+                response = await client.GetAsync(endPoint);
+                String strResult = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"狀態碼 : {response.StatusCode} ({(int)response.StatusCode})");
+                Console.WriteLine($"取得結果 : {strResult}");
+                onlyUserDto = JsonConvert.DeserializeObject<OnlyUserDto>(strResult,
+                    new JsonSerializerSettings { MetadataPropertyHandling = MetadataPropertyHandling.Ignore });
+            }
+
+            return onlyUserDto;
+        }
+    }
+
+    #region 呼叫 Web API 會用到的類別
+    public class OnlyUserDto
+    {
+        public string Message { get; set; }
+    }
+    #endregion
+}
---
+++ Examples/Blazor 認證、授權與 Web API 設計 範例原始碼.md
@@ -0,0 +1,25 @@
+# Blazor 認證、授權與 Web API 設計 範例原始碼
+
+* AA01 使用 HttpClient 來取得 存取權杖 Token(精簡版)
+* AA02 使用 HttpClient 來取得 存取權杖 Token (設計版)
+* AA03 使用 FormUrlEncodedContent (x-www-form-urlencoded) 產生要 Post 的資料
+* AA04 使用 MultipartFormDataContent (form-data) 產生要 Post 的資料
+* AA05 使用 JSON (Raw > JSON) 產生要 Post 的資料
+* AA06 使用 QueryString
+* AA07 使用 Route 資料
+* AA08 使用 POST 要求與使用 Header 進行傳送與接收 呼叫 Web API
+* AA09 使用 POST 要求與使用 Cookie 進行傳送與接收 呼叫 Web API
+* AA10 記錄下 HttpClient 的傳輸協定內容
+* AA11
+* AA12 沒有經過身分驗證，呼叫有授權才能存取的 API
+* AA13
+* AA14
+* AA15
+* AA16
+* AA17
+* AA18
+* AA19
+* AA1 
+* AA1 
+* AA1 
+
