6622f70a85c2e32a09287ba360b85595afbcb455
Vulcan <vulcan.lee@gmail.com>
2021-08-07 20:59:28 +0800

    Web API 送出不同資料之 HttpClient 作法 3 (Header)

---
+++ Examples/AA16/AA16/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace AA16
+{
+    /// <summary>
+    /// Web API 送出不同資料之 HttpClient 作法 3 (Header)
+    /// </summary>
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string result = "";
+            using (HttpClient client = new HttpClient())
+            {
+                string endPoint = $"https://localhost:5001/api/SendData/Header";
+
+                HttpResponseMessage response = await client.GetAsync(endPoint);
+                result = await response.Content.ReadAsStringAsync();
+
+                HttpHeaders headers = response.Headers;
+                IEnumerable<string> values;
+                string totalCount = "";
+                if (headers.TryGetValues("X-Total-Count", out values))
+                {
+                    totalCount = values.First();
+                }
+                Console.WriteLine($"此次呼叫取得內容 {result}");
+                Console.WriteLine($"X-Total-Count : {totalCount}");
+            }
+        }
+    }
+}
--- Examples/Blazor 認證、授權與 Web API 設計 範例原始碼.md
+++ Examples/Blazor 認證、授權與 Web API 設計 範例原始碼.md
@@ -15,7 +15,7 @@
 * AA13 使用存取權杖來呼叫有授權才能存取的 API
 * AA14 Web API 送出不同資料之 HttpClient 作法 1 (Body)
 * AA15 Web API 送出不同資料之 HttpClient 作法 2 (Cookie)
-* AA16
+* AA16 Web API 送出不同資料之 HttpClient 作法 3 (Header)
 * AA17
 * AA18
 * AA19
