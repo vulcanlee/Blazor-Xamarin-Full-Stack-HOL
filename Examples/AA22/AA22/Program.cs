df600fbe0c9075a08f36171672678d7e3598dbec
Vulcan <vulcan.lee@gmail.com>
2021-08-07 22:08:45 +0800

    AA22 HttpClient 讀取不同狀態碼與訊息內容

---
+++ Examples/AA22/AA22/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AA22
+{
+    /// <summary>
+    /// HttpClient 讀取不同狀態碼與訊息內容
+    /// </summary>
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string result = "";
+            using (HttpClient client = new HttpClient())
+            {
+                //string endPoint = $"https://localhost:5001/api/SendStatus/Ok";
+                //string endPoint = $"https://localhost:5001/api/SendStatus/NoContent";
+                //string endPoint = $"https://localhost:5001/api/SendStatus/NotFound";
+                //string endPoint = $"https://localhost:5001/api/SendStatus/BadRequest";
+                //string endPoint = $"https://localhost:5001/api/SendStatus/Unauthorized";
+                string endPoint = $"https://localhost:5001/api/SendStatus/CustomStausCode";
+
+                HttpResponseMessage response = await client.GetAsync(endPoint);
+                result = await response.Content.ReadAsStringAsync();
+                Console.WriteLine($"此次呼叫取得狀態碼 {response.StatusCode} ({(int)response.StatusCode})");
+                Console.WriteLine($"此次呼叫取得內容 {result}");
+            }
+
+        }
+    }
+}
--- Examples/Blazor 認證、授權與 Web API 設計 範例原始碼.md
+++ Examples/Blazor 認證、授權與 Web API 設計 範例原始碼.md
@@ -10,7 +10,7 @@
 * AA08 HttpClient 送出不同資料作法 6 (Cookie)
 * AA09 HttpClient 送出不同資料作法 7 (Header)
 * AA10 記錄下 HttpClient 的傳輸協定內容
-* AA11
+* AA11 使用 BaseWebAPI 類別來存取 Web API
 * AA12 沒有經過身分驗證，呼叫有授權才能存取的 API
 * AA13 使用存取權杖來呼叫有授權才能存取的 API
 * AA14 Web API 送出不同資料之 HttpClient 作法 1 (Body)
@@ -21,7 +21,7 @@
 * AA19 HttpClient 送出不同資料作法 8 (圖片檔)
 * AA20 HttpClient 使用 取消權杖 的程式設計方法
 * AA21 使用 取消權杖 的 時間逾期 程式設計方法
-* AA22 
+* AA22 HttpClient 讀取不同狀態碼與訊息內容
 * AA23 
 * AA24 
 * AA25 
