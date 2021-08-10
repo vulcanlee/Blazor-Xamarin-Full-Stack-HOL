c04f11d09f22bdffa46c9cda24bd5957d09650b7
Vulcan <vulcan.lee@gmail.com>
2021-08-10 21:34:12 +0800

    AA31 RestSharp 直接讀取回應字串

---
+++ Examples/AA31/AA31/Program.cs
@@ -0,0 +1,23 @@
+﻿using RestSharp;
+using System;
+using System.Threading.Tasks;
+
+namespace AA31
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var client = new RestClient($"https://localhost:5001");
+
+            //var request = new RestRequest("/api/SendStatus/OK", Method.GET);
+            //var request = new RestRequest("/api/SendStatus/NotFound", Method.GET);
+            //var request = new RestRequest("/api/SendStatus/BadRequest", Method.GET);
+            //var request = new RestRequest("/api/SendStatus/Unauthorized", Method.GET);
+            var request = new RestRequest("/api/SendStatus/CustomStausCode", Method.GET);
+
+            var response = await client.GetAsync<string>(request);
+            Console.WriteLine($"此次呼叫取得內容 {response}");
+        }
+    }
+}
