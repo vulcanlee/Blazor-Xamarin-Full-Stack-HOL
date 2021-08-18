eecc65750659b23d3003c5989eb4d5debfe7f8b6
Vulcan <vulcan.lee@gmail.com>
2021-08-19 01:22:27 +0800

    加入顯示 Route Table API

---
+++ Src/BackendStarter4/Backend/Controllers/MonitorController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using System.Linq;
+
+namespace Backend.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MonitorController : ControllerBase
+    {
+        private readonly IActionDescriptorCollectionProvider _provider;
+
+        public MonitorController(IActionDescriptorCollectionProvider provider)
+        {
+            _provider = provider;
+        }
+
+        [HttpGet("routes")]
+        public IActionResult GetRoutes()
+        {
+            var routes = _provider.ActionDescriptors.Items.Select(x => new {
+                Action = x.RouteValues["Action"],
+                Controller = x.RouteValues["Controller"],
+                Name = x.AttributeRouteInfo.Name,
+                Template = x.AttributeRouteInfo.Template
+            }).ToList();
+            return Ok(routes);
+        }
+    }
+}
