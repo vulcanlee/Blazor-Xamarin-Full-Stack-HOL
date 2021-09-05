41b219883ba2d6d1440910b0cc376d9721361033
Vulcan <vulcan.lee@gmail.com>
2021-09-06 00:38:04 +0800

    加入資料驗證

--- Examples/Blazor Quick HOL/Solution/Blogger/Models/BlogPost.cs
+++ Examples/Blazor Quick HOL/Solution/Blogger/Models/BlogPost.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,7 +9,9 @@ namespace Blogger.Models
     public class BlogPost
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="文章主題 必須存在")]
         public string Title { get; set; }
+        [Required(ErrorMessage ="文章內容 必須存在")]
         public string Text { get; set; }
         public DateTime PublishDate { get; set; }
         public Category Category { get; set; }
