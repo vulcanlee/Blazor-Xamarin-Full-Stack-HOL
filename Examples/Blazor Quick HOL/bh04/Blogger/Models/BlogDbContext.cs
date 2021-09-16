d9192656bf7ccf1e8ad7e7fd0a0ccde449d910ea
Vulcan <vulcan.lee@gmail.com>
2021-09-16 15:21:23 +0800

    修正 新增、修改、刪除 作業使用對話窗

---
+++ Examples/Blazor Quick HOL/bh04/Blogger/Models/BlogDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blogger.Models
+{
+    public class BlogDbContext : DbContext
+    {
+        public BlogDbContext(DbContextOptions<BlogDbContext> context) : base(context)
+        {
+        }
+        public DbSet<BlogPost> BlogPost { get; set; }
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Tag> Tag { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "Chinese_Taiwan_Stroke_CI_AS");
+
+            #region 設定階層級的刪除政策(預設若關聯子資料表有紀錄，父資料表不可強制刪除
+            foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+            {
+                relationship.DeleteBehavior = DeleteBehavior.Restrict;
+            }
+            #endregion
+        }
+    }
+}
