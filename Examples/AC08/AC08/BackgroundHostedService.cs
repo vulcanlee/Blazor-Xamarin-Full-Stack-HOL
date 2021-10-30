f2292aa71b3939211a41dee64246d5f58183efa0
Vulcan <vulcan.lee@gmail.com>
2021-10-30 08:45:19 +0800

    AC08 ASP.NET Core 應該具備知識 - 背景服務

---
+++ Examples/AC08/AC08/BackgroundHostedService.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AC08
+{
+    public class BackgroundHostedService : IHostedService
+    {
+        private readonly ILogger<BackgroundHostedService> logger;
+        DateTime StartupTime = DateTime.Now;
+        Task BackgroundHostedServiceTask;
+        CancellationTokenSource cancellationTokenSource = new();
+
+        public BackgroundHostedService(ILogger<BackgroundHostedService> logger)
+        {
+            this.logger = logger;
+        }
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            cancellationTokenSource = new CancellationTokenSource();
+            logger.LogInformation($"託管服務的背景工作 開始啟動");
+
+            var backgroundService = Task.Run(async () =>
+            {
+                try
+                {
+                    StartupTime = DateTime.Now;
+                    await Task.Delay(5000);
+
+                    while (cancellationTokenSource.Token.IsCancellationRequested == false)
+                    {
+                        logger.LogInformation($"託管服務的背景工作 正在執行中");
+                        logger.LogInformation($"託管服務的背景工作 等候下一次的執行");
+                        await Task.Delay(60*1000, cancellationTokenSource.Token);
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    logger.LogInformation($"託管服務的背景工作 準備正常離開中");
+                }
+                catch (Exception ex)
+                {
+                    logger.LogWarning(ex, $"託管服務的背景工作 產生例外異常");
+                }
+            });
+            BackgroundHostedServiceTask = backgroundService;
+
+            return Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            cancellationTokenSource.Cancel();
+            for (int i = 0; i < 10; i++)
+            {
+                if (BackgroundHostedServiceTask.IsCompleted == true)
+                    break;
+#pragma warning disable CA2016 // 將 'CancellationToken' 參數傳遞給使用該參數的方法
+                await Task.Delay(500);
+#pragma warning restore CA2016 // 將 'CancellationToken' 參數傳遞給使用該參數的方法
+            }
+            TimeSpan timeSpan = DateTime.Now - StartupTime;
+            logger.LogInformation($"託管服務的背景工作 即將停止，共花費 {timeSpan}");
+
+            return;
+        }
+    }
+}
---
+++ Examples/ASP.NET Core 應該具備知識.md
@@ -0,0 +1,55 @@
+# ASP.NET Core 應該具備知識 範例原始碼
+
+* AC01 ASP.NET Core 應該具備知識 - 應用程式啟動與Startup類別
+* AC02 ASP.NET Core 應該具備知識 - 靜態檔案
+* AC03 ASP.NET Core 應該具備知識 - 相依性注入設計模式
+* AC04 ASP.NET Core 應該具備知識 - 中介軟體
+* AC05 ASP.NET Core 應該具備知識 - 設定
+* AC06 ASP.NET Core 應該具備知識 - 選項模式
+* AC07 ASP.NET Core 應該具備知識 - 日誌記錄
+* AC08 ASP.NET Core 應該具備知識 - 背景服務
+* AC09 
+* AC10 
+* AC11 
+* AC12 
+* AC13 
+* AC14 
+* AC15 
+* AC16 
+* AC17 
+* AC18 
+* AC19 
+* AC20 
+* AC21 
+* AC22 
+* AC23 
+* AC24 
+* AC25 
+* AC26 
+* AC27 
+* AC28 
+* AC29 
+* AC30 
+* AC31 
+* AC32 
+* AC33 
+* AC34 
+* AC35 
+* AC36 
+* AC37 
+* AC38 
+* AC39 
+* AC40 
+* AC41 
+* AC42 
+* AC43 
+* AC44 
+* AC45 
+* AC46 
+* AC47 
+* AC48 
+* AC49 
+
+* AC99 ASP.NET Core 應該具備知識 - 從空白專案建立 Blazor 應用
+
+
